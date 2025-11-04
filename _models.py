8d8b8bf8632683ee95e8abd0cecfcf13c0a79b39
adrian.adduci <adrian.adduci@gmail.com>
2025-11-04 11:22:13 -0500

    Update Requirements

--- _models.py
+++ _models.py
@@ -2,7 +2,8 @@
 # Author: Adrian Adduci
 # Email: FAA2160@columbia.edu
 ################################################################################
-
+from dotenv import load_dotenv
+import os
 import logging
 import os
 import pathlib
